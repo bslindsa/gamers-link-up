07674e4cd8d015c09e6c271cc21ce85ec62f6033
Brad Lindsay <bslindsa@gmail.com>
2022-02-23 14:23:01 -0700

    Updated game model

---
+++ server/models/Game.js
@@ -0,0 +1,39 @@
+const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+const gameSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    owner: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: 'Please describe this product.',
+        minlength: 1,
+        maxlength: 280,
+        trim: true,
+    },
+    platform: {
+        type: String,
+    },
+    price: {
+        type: Number,
+    },
+    date_posted: {
+        type: Date,
+        default: Date.now,
+        get: (timestamp) => dateFormat(timestamp),
+    },
+    tags: [
+        {
+            name: {
+                type: String
+            }
+        }
+    ]
+})

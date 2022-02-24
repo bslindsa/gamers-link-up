b5b83b14ac2025b6a66bd7882ba3b22617540f71
Chris Jimenez <chrisjimenez8118@gmail.com>
2022-02-23 23:33:54 -0700

    Added User, Game and Tag type defs. Added user and game queries in resolvers.

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,27 +1,43 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-  type Tech {
-    _id: ID!
-    name: String!
+  type User {
+    _id: ID
+    username: String
+    email: String
+    password: String
+    games: [Game]!
+}
+
+  type Game {
+    _id: ID
+    title: String
+    owner: String
+    description: String
+    platform: String
+    price: Number
+    date_posted: String
+    tags: [Tag]!
   }
 
-  type Matchup {
-    _id: ID!
-    tech1: String!
-    tech2: String!
-    tech1_votes: Int
-    tech2_votes: Int
+  type Tag {
+    _id: ID
+    name: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
   }
 
   type Query {
-    tech: [Tech]
-    matchups(_id: String): [Matchup]
+    users: [User]
+    user(username: String!): User
   }
 
   type Mutation {
-    createMatchup(tech1: String!, tech2: String!): Matchup
-    createVote(_id: String!, techNum: Int!): Matchup
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
   }
 `;
 

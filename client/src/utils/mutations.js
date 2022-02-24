9a17c1460025bc7e16f8b502d327fe30b41d8f0d
Brad Lindsay <bslindsa@gmail.com>
2022-02-24 14:49:05 -0700

    Mutations and queries. Include editGame and removeTag

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,23 +1,77 @@
 import { gql } from '@apollo/client';
 
-export const CREATE_MATCHUP = gql`
-  mutation createMatchup($tech1: String!, $tech2: String!) {
-    createMatchup(tech1: $tech1, tech2: $tech2) {
+export const ADD_USER = gql`
+mutation addUser($username: String!, $email: String!, $password: String!) {
+  addUser(username: $username, email: $email, password: $password) {
+    token
+    user {
       _id
-      tech1
-      tech2
+      username
     }
   }
+}
 `;
 
-export const CREATE_VOTE = gql`
-  mutation createVote($_id: String!, $techNum: Int!) {
-    createVote(_id: $_id, techNum: $techNum) {
+export const LOGIN = gql`
+mutation login($email: String!, $password: String!) {
+  login(email: $email, password: $password) {
+    token
+    user {
       _id
-      tech1
-      tech2
-      tech1_votes
-      tech2_votes
+      username
     }
   }
+}
+`;
+
+export const ADD_GAME = gql`
+mutation addGame($title: String!, $description: String, $platform: String) {
+  addGame(title: $title, description: $description, platform: $platform) {
+    _id
+    title
+    description
+    platform
+  }
+}
+`;
+
+export const ADD_TAG = gql`
+mutation addTag($gameId: ID!, $tagName: String!) {
+  addTag(gameId: $gameId, tagName: $tagName) {
+    title
+    tags {
+      tagName
+    }
+  }
+}
+`;
+
+export const REMOVE_TAG = gql`
+mutation removeTag($gameId: ID!, $tagName: String!) {
+  title
+  tags {
+    tagName
+  }
+}
+`;
+
+export const EDIT_GAME = gql`
+mutation editGame($gameId: ID!, $title: String, $description: String, $platform: String) {
+  editGame(gameId: $gameId, title: $title, description: $description, platform: $platform) {
+    _id
+    title
+    description
+    platform
+  }
+}
+
+`;
+
+export const DELETE_GAME = gql`
+mutation deleteGame($gameId: ID!) {
+  deleteGame(gameId: $gameId) {
+    _id
+    title
+  }
+}
 `;

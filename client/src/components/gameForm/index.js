d27a32bc229bdb8e82d0641fef6d2f240638600f
Brad Lindsay <bslindsa@gmail.com>
2022-02-28 11:54:41 -0700

    Update icon

--- client/src/components/gameForm/index.js
+++ client/src/components/gameForm/index.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useMutation } from '@apollo/client';
-// import $ from 'jquery';
 
 import { ADD_GAME } from '../../utils/mutations';
 
@@ -30,40 +29,9 @@ const GameForm = () => {
                 (file) => URL.revokeObjectURL(file)
             )
             console.log(selectedImages);
-
-        };
-
-
-        // const handleNumberChange = (event) => {
-        //     const { name, value} = event.target;
-            
-        //     setFormState({...formState})
-        // }
-
-        const handleFormSubmit = async (event) => {
-            event.preventDefault();
-            console.log(formState)
-            // console.log(title);
-            try {
-                // eslint-disable-next-line
-                const {data} = await addGame({
-                    variables: { ...formState, price: parseFloat(formState.price) },
-                });
-                setFormState({
-                    title: '',
-                    description: '',
-                    platform: '',
-                    price: 0
-                });
-            } catch (err) {
-                console.log('catch');
-                console.error(err);
-            }
         };
 
         setFormState({ ...formState, [name]: value });
-        // console.log(selectedImages);
-        // console.log(formState.images);
     };
 
 
@@ -95,64 +63,6 @@ const GameForm = () => {
             return <img className='preview m-2' src={photo} key={photo} alt='Preview' />
         })
     }
-
-
-        // return(
-        // <main className = "flex-row justify-center mb-4" >
-        //     {
-        //         Auth.loggedIn() ?  (
-        //             <>
-        //                 <div className="col-12 col-lg-10">
-        //                     <div className="card">
-        //                         <h4 className="card-header bg-dark text-light p-2">Game</h4>
-        //                         <div className="card-body">
-        //                             <form onSubmit={handleFormSubmit}>
-        //                                 <input
-        //                                     className="form-input"
-        //                                     placeholder="Game Title"
-        //                                     name="title"
-        //                                     type="text"
-        //                                     value={formState.title}
-        //                                     onChange={handleChange}
-        //                                 />
-        //                                 <input
-        //                                     className="form-input"
-        //                                     placeholder="Game Description"
-        //                                     name="description"
-        //                                     type="text"
-        //                                     value={formState.description}
-        //                                     onChange={handleChange}
-        //                                 />
-        //                                 <input
-        //                                     className="form-input"
-        //                                     placeholder="Platform"
-        //                                     name="platform"
-        //                                     type="text"
-        //                                     value={formState.platform}
-        //                                     onChange={handleChange}
-        //                                 />
-        //                                 <input
-        //                                     className="form-input"
-        //                                     placeholder="Price"
-        //                                     name="price"
-        //                                     pattern="[0-9]*"
-        //                                     type="text"
-        //                                     value={formState.price}
-        //                                     onChange={handleChange}
-        //                                 />
-        //                                 <button
-        //                                     className="btn btn-block btn-primary"
-        //                                     style={{ cursor: 'pointer' }}
-        //                                     type="submit"
-        //                                 >
-        //                                     Submit
-        //                                 </button>
-        //                             </form>
-        //                             {error && (
-        //                                 <div className="my-3 p-3 bg-danger text-white">
-        //                                     {error.message}
-        //                                 </div>
-        //                             )}
 
     return (
         <main>

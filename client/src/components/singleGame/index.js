64c5f485bb6b055681233ecc687314bc1cd60f41
Brad Lindsay <bslindsa@gmail.com>
2022-02-27 23:51:29 -0700

    Styling and organizing

--- client/src/components/singleGame/index.js
+++ client/src/components/singleGame/index.js
@@ -34,7 +34,7 @@ const SingleGame = () => {
                     <div>
                         <h1>This one's a beauty... if you've the coin.</h1>
                     </div>
-                    <div key={game.title} className="game col-5 mb-5">
+                    <div key={game.title} className="game">
                         <div className="card">
                             <div className="title">
                                 <h3>{game.title}</h3>

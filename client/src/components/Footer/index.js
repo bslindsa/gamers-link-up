4e524357d3a92c3e854b16fbb3a32dcc9d475d0c
Brad Lindsay <bslindsa@gmail.com>
2022-04-01 15:37:29 -0700

    Add chat toggle button. Not currently functional

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -4,13 +4,24 @@ import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
-    <footer className="my-footer">
-      <div className="container text-center">
-        <Link className="text-light" to={"/"}>
-          <button className="btn btn-dark">Games</button>
-        </Link>
+    <div>
+      <div className='collapse' id='chat-list'>
+        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
       </div>
-    </footer>
+      <footer className="my-footer">
+        <div>
+          <button className="container pop-btn" data-toggle='collapse' data-target='#chat-list' role='button' aria-expanded='false' aria-controls="chat-list">
+            Messages
+          </button>
+        </div>
+        <div className="container text-center">
+          <Link className="text-light" to={"/"}>
+            <button className="btn btn-dark">Games</button>
+          </Link>
+        </div>
+      </footer>
+    </div>
+
   );
 };
 
